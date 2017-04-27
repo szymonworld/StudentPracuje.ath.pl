62fbd3406bd5968301f1a8e2db47f77ed00678ac
Szymon Holisz <szymonworld@gmail.com>
2017-04-27 10:50:40 +0200

    Fix + Editor

---
+++ studentpracuje.ath.bielsko.pl/Pracuj.ath.bielsko.pl/Controllers/EditorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pracuj.ath.bielsko.pl.Controllers
+{
+    public class EditorController : Controller
+    {
+        // GET: Editor
+        public ActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
