816b71fe7896cdbaa545a5473d438b75d0dc8f85
GXD12345 <swmail45@gmail.com>
2017-05-04 10:02:58 +0200

    Dodano HelperEditor

---
+++ studentpracuje.ath.bielsko.pl/Pracuj.ath.bielsko.pl/Helpers/EditorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pracuj.ath.bielsko.pl.Helpers
+{
+    public static class EditorExtensions
+    {
+        public static MvcHtmlString TextEditor(this HtmlHelper helper, string target)
+        {
+            return MvcHtmlString.Create("<script> $(function() { $('#" + target + "').ckeditor(); }); </script> <textarea id='" + target + "' style='padding-top:50px;'></textarea>");
+        }
+    }
+}
